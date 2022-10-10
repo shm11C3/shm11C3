f6630ca377db0cfe7a4bc6812e396b7c1693888f
shm <s.murakami.tech@gmail.com>
2022-10-11 03:14:57 +0900

    Update m11c3.sh

--- m11c3.sh
+++ m11c3.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
+# Windows Only
 
-curl 'https://github.com/shm11C3'
+function confirm {
+  while true; do
+    echo -n "$* [y/n]: "
+    read CONFIRM_RESULT
+    case $CONFIRM_RESULT in
+      [Yy]*)
+        return 0
+        ;;
+      [Nn]*)
+        return 1
+        ;;
+      *)
+        echo "y | n"
+        ;;
+    esac
+  done
+}
+
+if confirm "Want to send me an email?"; then
+  start 'mailto:m11c3.sh@gmail.com'
+fi
+
+if confirm "Do you want to read my code?"; then
+  start 'https://github.com/shm11C3'
+fi
+
+echo "bye"
+
+sleep 1
